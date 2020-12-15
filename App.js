0a54ebb86894ff43f86de5ad4bf554778260512e
Yai <afroyogo@icloud.com>
2020-12-15 23:47:11 +0100

    Added routing system and two pages

--- App.js
+++ App.js
@@ -5,67 +5,73 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  Image,
-} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from "react-native";
 
-import styles from '@styles/styles';
+// Importación de las vistas
+import LandingPage from '@screens/LandingPage';
+import LoginScreen from '@screens/LoginScreen';
+
+const RootStack = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionImageContainer}>
-              <View style={styles.imageContainer}>
-                <Text style={styles.sectionTitle}>Bienvenidos</Text>
+     <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="BienvenidaScreen" component={LandingPage} />
+        <RootStack.Screen name="LoginScreen" component={LoginScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+      {/* <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.body}>
+              <View style={styles.sectionImageContainer}>
+                <View style={styles.imageContainer}>
+                  <Text style={styles.sectionTitle}>Bienvenidos</Text>
+                  <View style={styles.ButtonContainer}>
+                    <Image
+                      styles={styles.form}
+                      source={require('./screenstest/descarga.png')}
+                    />
+                  </View>
+                  <Text tyle={styles.sectionMessage}>
+                    Entra a Un lugar donde puedes ser
+                </Text>
+                </View>
+              </View>
+              <View style={styles.sectionContainerButton}>
                 <View style={styles.ButtonContainer}>
-                  <Image
-                    styles={styles.form}
-                    source={require('./screenstest/descarga.png')}
+                  <Button
+                    onPress={() => {
+                      console.log('you tapped the button Iniciar');
+                    }}
+                    title="INICIAR SESIÓN"
                   />
                 </View>
-                <Text tyle={styles.sectionMessage}>
-                  Entra a Un lugar donde puedes ser
-                </Text>
               </View>
-            </View>
-            <View style={styles.sectionContainerButton}>
-              <View style={styles.ButtonContainer}>
-                <Button
-                  onPress={() => {
-                    console.log('you tapped the button Iniciar');
-                  }}
-                  title="INICIAR SESIÓN"
-                />
-              </View>
-            </View>
 
-            <View style={styles.sectionContainerButton1}>
-              <View style={styles.ButtonContainer}>
-                <Button
-                  onPress={() => {
-                    console.log('you tapped the button regist');
-                  }}
-                  title="REGISTRARSE "
-                />
+              <View style={styles.sectionContainerButton1}>
+                <View style={styles.ButtonContainer}>
+                  <Button
+                    onPress={() => {
+                      console.log('you tapped the button regist');
+                    }}
+                    title="REGISTRARSE "
+                  />
+                </View>
               </View>
             </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+          </ScrollView>
+        </SafeAreaView>
+      </NavigationContainer> */}
     </>
   );
 };
