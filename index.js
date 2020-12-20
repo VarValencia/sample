83735c80307be26b6a107f331eca111308f14eda
afroyogo <jaijaudenes@gmail.com>
2020-12-20 21:42:43 +0100

    Added global GudText cmponent

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import GudText from './components/GudText'
+import { name as appName } from './app.json';
 
+global.GudText = GudText;
 AppRegistry.registerComponent(appName, () => App);
