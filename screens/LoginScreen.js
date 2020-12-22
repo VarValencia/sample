895f3f61eda808e230fcf59fabc70a639b459a08
Valencia <cvalencias1995@gmail.com>
2020-12-22 10:15:44 +0100

    clen, dry and change to GudAssets (Text=>GudText, .textInput=>gudText,TextLogSc => gudTextUpButton, ButtonTextLogSc=> gudButtonDark, expecting more changes)

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,61 +7,56 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  TouchableHighlight,
-  TextInput,
-  Image,
-} from 'react-native';
-
+import {View, StatusBar, Image, TextInput} from 'react-native';
 import styles from '@styles/styles';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {TouchableHighlight} from 'react-native-gesture-handler';
+import GudText from '../components/GudText';
 
 const LoginScreen: () => React$Node = () => {
   return (
     <>
       <StatusBar hidden={true} />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionContainerMarginTop}>
-              <View>
-                <Image
-                  style={styles.logo}
-                  source={require('../iconos_app/1x/GUDLogo.png')}
-                />
-              </View>
-              <Text style={styles.sectionTitle}>¡BIENVENIDO!</Text>
-              <Text style={styles.sectionDescription}>
-                Introduce Email y contraseña para acceder a tu cuenta
-              </Text>
+        <View style={styles.body}>
+          <View style={styles.cardContainer}>
+            <View style={styles.appImageContainer}>
+              <Image
+                style={styles.logo}
+                source={require('../iconos_app/1x/GUDLogo.png')}
+              />
             </View>
-            <View style={styles.Container}>
-              <View style={styles.sectionContainerLogSc}>
-                <Text style={styles.TextLogSc}>Email</Text>
-                <TextInput style={styles.textInput1} editable maxLength={40} />
-              </View>
-              <View style={styles.sectionContainerLogSc}>
-                <Text style={styles.TextLogSc}>Contraseña</Text>
-                <TextInput style={styles.textInput1} editable maxLength={40} />
-              </View>
-              <View style={styles.ButtonLogSc}>
-                <TouchableHighlight
-                  onPress={() => {
-                    console.log('you tapped the button ACCEDER');
-                  }}>
-                  <Text style={styles.ButtonTextLogSc}>Acceder</Text>
-                </TouchableHighlight>
-              </View>
+            <GudText
+              style={[styles.GudText, styles.sectionTitle]}
+              text="¡BIENVENIDO"
+            />
+            <GudText
+              style={[styles.GudText, styles.sectionDescription]}
+              text="Introduce Email y contraseña para acceder a tu cuenta"
+            />
+          </View>
+          <View style={styles.Container}>
+            <View style={styles.sectionContainerLogSc}>
+              <GudText
+                style={[styles.GudText, styles.TextLogSc]}
+                text="Email"
+              />
+              <TextInput style={styles.gudInput1} editable maxLength={40} />
+            </View>
+            <View style={styles.gudContainierBigMargin}>
+              <GudText style={[styles.TextUpButton]} text="Contraseña" />
+              <TextInput style={styles.gudInput} editable maxLength={40} />
+            </View>
+            <View style={styles.ButtonLogSc}>
+              <TouchableHighlight
+                onPress={() => {
+                  console.log('you tapped the button ACCEDER');
+                }}>
+                <GudText style={styles.GudButtonDark} text="Login" />
+              </TouchableHighlight>
             </View>
           </View>
-        </ScrollView>
+        </View>
       </SafeAreaView>
     </>
   );
