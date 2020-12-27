d006644fc829a1f17f34e880ae5a53a266c7d9b2
afroyogo <jaijaudenes@gmail.com>
2020-12-27 12:29:42 +0100

    Created class component

--- screens/Landing/LoginScreen.js
+++ screens/Landing/LoginScreen.js
@@ -13,66 +13,132 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {TouchableHighlight} from 'react-native-gesture-handler';
 import GudText from '../../components/GudText';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import ValidationField from '../../components/ValidationField';
 
-const LoginScreen: () => React$Node = ({navigation}) => {
-  return (
-    <>
-      <StatusBar hidden={true} />
-      <SafeAreaView>
-        <View style={styles.body}>
-          <View style={styles.cardContainer}>
-            <View style={styles.appImageContainer}>
-              <Image source={require('@icons/1x/GUDLogo.png')} />
-            </View>
-            <GudText
-              style={[styles.title, styles.textLG]}
-              text="¡BIENVENIDO!"
-            />
-            <View style={styles.gudSeparator} />
-            <GudText
-              style={[styles.sectionDescription, styles.textLG]}
-              text="Introduce Email y contraseña para acceder a tu cuenta"
-            />
-          </View>
-          <View style={styles.bottomContainer}>
-            <View style={styles.inputContainer}>
-              <GudText style={[styles.textSM, styles.gudInputText]} text="Email" />
-              <TextInput
-                style={styles.gudInput}
-                maxLength={40}
-                placeholder="Email"
-              />
+class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFormFilled: false,
+      email: '',
+      password: '',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.navigateTo = this.navigateTo.bind(this);
+    this.validate = this.validate.bind(this);
+  }
 
-              <GudText style={[styles.textSM, styles.gudInputText]} text="Contraseña" />
-              <TextInput
-                style={styles.gudInput}
-                maxLength={40}
-                placeholder="Contraseña"
+  // User functions
+  onChange(str, ref) {
+    switch (ref) {
+      case 0:
+        this.setState({email: str});
+        break;
+      case 1:
+        this.setState({password: str});
+        break;
+    }
+    this.validate();
+  }
+  validate = () => {
+    if (!this.isFormFilled && this.state.email && this.state.password) {
+      this.setState({isFormFilled: true});
+    } else {
+      this.setState({isFormFilled: false});
+    }
+  };
+  navigateTo(screen) {
+    this.props.navigation.navigate(screen);
+  }
+
+  render() {
+    const EMAIL = 0;
+    const PASS = 1;
+    console.log('Parent', this.state);
+    return (
+      <>
+        <StatusBar hidden={true} />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <View style={styles.cardContainer}>
+              <View style={styles.appImageContainer}>
+                <Image source={require('@icons/1x/GUDLogo.png')} />
+              </View>
+              <GudText
+                style={[styles.title, styles.textLG]}
+                text="¡BIENVENIDO!"
+              />
+              <View style={styles.gudSeparator} />
+              <GudText
+                style={[styles.sectionDescription, styles.textLG]}
+                text="Introduce Email y contraseña para acceder a tu cuenta"
               />
             </View>
-            <View style={styles.buttonContainer}>
-              <View style={[styles.gudButton, styles.active]}>
-                <TouchableHighlight
-                  style={styles.touchableActive}
-                  underlayColor={EStyleSheet.value('$gudGreenMedium')}
-                  onPress={() => {
-                    navigateTo('LandingPage');
-                  }}>
-                  <GudText
-                    style={[styles.gudButtonText, styles.textLG]}
-                    text="Acceder"
+            <View style={styles.bottomContainer}>
+              <View style={styles.inputContainer}>
+                {/* <GudText
+                  style={[styles.textSM, styles.gudInputText]}
+                  text="Email"
+                />
+                <TextInput
+                  style={styles.gudInput}
+                  maxLength={40}
+                  placeholder="Email"
+                /> */}
+                <ValidationField
+                  placeholder={'Email'}
+                  maxLength={30}
+                  minLength={6}
+                  handleValue={(event) => this.onChange(event, EMAIL)}
+                />
+                {/* <GudText
+                  style={[styles.textSM, styles.gudInputText]}
+                  text="Contraseña"
                   />
-                </TouchableHighlight>
+                  <TextInput
+                  style={styles.gudInput}
+                  maxLength={40}
+                  placeholder="Contraseña"
+                /> */}
+                <ValidationField
+                  placeholder={'Contraseña'}
+                  maxLength={30}
+                  minLength={6}
+                  handleValue={(event) => this.onChange(event, PASS)}
+                  password={true}
+                />
+              </View>
+              <View style={styles.buttonContainer}>
+                <View
+                  style={[
+                    styles.gudButton,
+                    this.state.isFormFilled
+                      ? styles.activeBtn
+                      : styles.inactiveBtn,
+                  ]}>
+                  <TouchableHighlight
+                    disabled={!this.state.isFormFilled}
+                    style={styles.touchableActive}
+                    underlayColor={EStyleSheet.value('$gudGreenMedium')}
+                    onPress={() => {
+                      this.navigateTo('LandingPage');
+                    }}>
+                    <GudText
+                      style={[
+                        styles.gudButtonText,
+                        styles.textMD,
+                        this.state.isFormFilled ? null : styles.inactiveText,
+                      ]}
+                      text="Acceder"
+                    />
+                  </TouchableHighlight>
+                </View>
               </View>
             </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </>
-  );
-  function navigateTo(screen) {
-    navigation.navigate(screen);
+        </SafeAreaView>
+      </>
+    );
   }
-};
-
+}
 export default LoginScreen;
