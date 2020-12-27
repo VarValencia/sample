8041809f4e790722950bdd66ffe1d45f31d598a0
afroyogo <jaijaudenes@gmail.com>
2020-12-27 11:08:08 +0100

    Created class component (RegisterStepTwo)

--- screens/Landing/RegisterStepTwo.js
+++ screens/Landing/RegisterStepTwo.js
@@ -12,62 +12,73 @@ import styles from '@styles/styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {TouchableHighlight} from 'react-native-gesture-handler';
 import GudText from '../../components/GudText';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-const RegisterStepTwo: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar hidden={true} />
-      <SafeAreaView>
-        <View style={styles.body}>
-          <View style={styles.cardContainer}>
-            <View style={styles.appImageContainer}>
-              <Image source={require('@icons/1x/GUDLogo.png')} />
-            </View>
-            <GudText
-              style={[styles.title, styles.textMD]}
-              text="Completa tu cuenta"
-            />
-            <View style={styles.gudContainerBigMargin}>
+class RegisterStepTwo extends React.Component {
+  constructor(props) {
+    super(props),
+    this.state = {
+      isFormFilled: true,
+      name: '',
+      birthday: '',
+      country: '',
+      language: ''
+    }
+  }
+  render() {
+    return (
+      <>
+        <StatusBar hidden={true} />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <View style={styles.cardContainer}>
+              <View style={styles.appImageContainer}>
+                <Image source={require('@icons/1x/GUDLogo.png')} />
+              </View>
               <GudText
-                style={[styles.GudText, styles.gudTextUpButton]}
-                text="Nombre de usuario"
+                style={[styles.title, styles.textLG]}
+                text="Completa tu cuenta para finalizar el registro"
               />
-              <TextInput style={styles.gudInput} maxLength={40} />
             </View>
-            <View style={styles.gudContainerBigMargin}>
-              <GudText
-                style={[styles.GudText, styles.gudTextUpButton]}
-                text="Fecha de nacimiento"
-              />
-              <TextInput style={styles.gudInput} maxLength={40} />
-            </View>
-            <View style={styles.gudContainerBigMargin}>
-              <GudText
-                style={[styles.GudText, styles.gudTextUpButton]}
-                text="Nacionalidad"
-              />
-              <TextInput style={styles.gudInput} maxLength={40} />
-            </View>
-            <View style={styles.gudContainerBigMargin}>
-              <GudText
-                style={[styles.GudText, styles.gudTextUpButton]}
-                text="Idioma"
-              />
-              <TextInput style={styles.gudInput} maxLength={40} />
-            </View>
-            <View style={styles.gudButtonDark}>
-              <TouchableHighlight
-                onPress={() => {
-                  console.log('you tapped the button ACCEDER');
-                }}>
-                <GudText style={styles.gudButtonDark} text="Acceder" />
-              </TouchableHighlight>
+            <View style={styles.bottomContainer}>
+              <View style={styles.inputContainer}>
+                <GudText style={styles.textMD} text="Nombre de usuario" />
+                <TextInput style={styles.gudInput} maxLength={40} />
+                <GudText
+                  style={[styles.GudText, styles.gudTextUpButton]}
+                  text="Fecha de nacimiento"
+                />
+                <TextInput style={styles.gudInput} maxLength={40} />
+                <GudText
+                  style={[styles.GudText, styles.gudTextUpButton]}
+                  text="Nacionalidad"
+                />
+                <TextInput style={styles.gudInput} maxLength={40} />
+                <GudText
+                  style={[styles.GudText, styles.gudTextUpButton]}
+                  text="Idioma"
+                />
+                <TextInput style={styles.gudInput} maxLength={40} />
+              </View>
+              <View style={styles.buttonContainer}>
+                <View style={styles.gudButton}>
+                  <TouchableHighlight
+                    disabled={!this.state.isFormFilled}
+                    style={styles.touchableActive}
+                    underlayColor={EStyleSheet.value('$gudGreenMedium')}
+                    onPress={() => {
+                      console.log('you tapped the button ACCEDER');
+                    }}>
+                    <GudText style={styles.gudButtonText} text="Acceder" />
+                  </TouchableHighlight>
+                </View>
+              </View>
             </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </>
-  );
-};
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 export default RegisterStepTwo;
