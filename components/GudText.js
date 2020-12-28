8d319cd850155735899cff7b1d08f6a9bc6b38bc
afroyogo <jaijaudenes@gmail.com>
2020-12-28 12:20:54 +0100

    GudText component can now accentuate words

--- components/GudText.js
+++ components/GudText.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {Text} from 'react-native';
 import styles from '@styles/styles';
 
 const GudText = (props) => {
-    return (
+  const originalText = props.text.trim().slice(0, props.text.lastIndexOf(' '));
+  const accent = props.text.slice(props.text.lastIndexOf(' ') + 1);
+  return (
+    <>
+      {props.accent ? (
+        <Text style={[styles.gudText, props.style]}>
+          {originalText} <Text style={styles.gudButtonText}>{accent}</Text>
+        </Text>
+      ) : (
         <Text style={[styles.gudText, props.style]}>{props.text}</Text>
-    );
-}
+      )}
+    </>
+  );
+};
 
 export default GudText;
