7553c599170943e1513f6c5411b5b1529b9cba21
afroyogo <jaijaudenes@gmail.com>
2021-01-02 23:48:41 +0100

    Can now adjust different accents in GudText component

--- components/GudText.js
+++ components/GudText.js
@@ -5,19 +5,42 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 const GudText = (props) => {
   const originalText = props.text.trim();
-  const regularText = originalText.slice(0, originalText.lastIndexOf(' '));
-  const accent = originalText.slice(originalText.lastIndexOf(' ') + 1);
-  return (
-    <>
-      {props.accent ? (
+  if (props.slice) {
+    var start = props.slice[0];
+    var end = props.slice[1];
+    const accent = originalText.slice(start, end);
+    const beforeString = originalText.slice(0, start);
+    const afterString = originalText.slice(end);
+
+    return (
+      <>
         <Text style={[styles.gudText, props.style]}>
-          {regularText} <Text style={{color: EStyleSheet.value('$gudGreenDark')}}>{accent}</Text>
+          {beforeString}
+          <Text style={{color: EStyleSheet.value('$gudGreenDark')}}>
+            {accent}
+          </Text>
+          {afterString}
         </Text>
-      ) : (
-        <Text style={[styles.gudText, props.style]}>{originalText}</Text>
-      )}
-    </>
-  );
+      </>
+    );
+  } else {
+    const regularText = originalText.slice(0, originalText.lastIndexOf(' '));
+    const accent = originalText.slice(originalText.lastIndexOf(' ') + 1);
+    return (
+      <>
+        {props.accent ? (
+          <Text style={[styles.gudText, props.style]}>
+            {regularText}{' '}
+            <Text style={{color: EStyleSheet.value('$gudGreenDark')}}>
+              {accent}
+            </Text>
+          </Text>
+        ) : (
+          <Text style={[styles.gudText, props.style]}>{originalText}</Text>
+        )}
+      </>
+    );
+  }
 };
 
 export default GudText;
