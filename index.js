8e08ac6ee48fa6874fe943cbaea63b896b6be51b
afroyogo <jaijaudenes@gmail.com>
2021-01-02 13:46:07 +0100

    Updated global scripts

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ import GRadioButtonGroup from '@components/GRadioButtonGroup';
 import ValidationField from '@components/ValidationField';
 import { name as appName } from './app.json';
 
+import AuthenticationService from '@scripts/authentication/AuthenticationService'
+
+// Global components
 global.GudText = GudText;
 global.GRadioButtonGroup = GRadioButtonGroup;
 global.ValidationField = ValidationField;
+
+// Global scripts
+global.AuthenticationService = AuthenticationService;
 
 AppRegistry.registerComponent(appName, () => App);
