c28e703a404a446b6b5b1fb369f71e3acd3c0e06
afroyogo <jaijaudenes@gmail.com>
2021-01-03 00:06:23 +0100

    Implemented new GudText .slice property to accentuate specific substrings inside a text

--- components/MatchConfigurator.js
+++ components/MatchConfigurator.js
@@ -21,15 +21,24 @@ class MatchConfigurator extends React.Component {
     this.props.onChange({ref, key});
   }
   buildString(screen) {
-    return (
-      'Selecciona qué ' +
-      this.state.strings[screen] +
-      ' eliges para este match:'
-    );
+    var prebuiltString = [
+      'Selecciona qué ',
+      this.state.strings[screen],
+      ' eliges para este match:',
+    ];
+    var processedString = prebuiltString.join('');
+    var accentEnd = processedString.indexOf(prebuiltString[2].substring(0, 5))
+
+    return {processedString, accentEnd};
   }
+
+  getAccentEnd(key){
+    return this.buildString(key).accentEnd;
+  }
+  
   render() {
     const MENUS = ['privacy', 'language', 'age'];
-
+    
     let privacy = [
       {
         name: 'Chat',
@@ -97,8 +106,9 @@ class MatchConfigurator extends React.Component {
             return (
               <View style={styles.carouselCard}>
                 <GudText
-                  style={{flexShrink: 1, padding: 20}}
-                  text={this.buildString(item.key)}
+                  style={[{flexShrink: 1, padding: 20}, styles.textMD]}
+                  slice={[15, this.getAccentEnd(item.key)]}
+                  text={this.buildString(item.key).processedString}
                 />
                 <GRadioButtonGroup
                   options={item.value}
