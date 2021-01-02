5e341c29d4b6a370493c3e8fffcf69c74eab48c7
afroyogo <jaijaudenes@gmail.com>
2021-01-02 16:39:50 +0100

    Globalised components and styles

--- index.js
+++ index.js
@@ -4,17 +4,30 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { name as appName } from './app.json';
+
+// Component imports
 import GudText from '@components/GudText';
 import GRadioButtonGroup from '@components/GRadioButtonGroup';
 import ValidationField from '@components/ValidationField';
-import { name as appName } from './app.json';
+import CarouselCard from '@components/CarouselCard';
 
+// Scripts import
 import AuthenticationService from '@scripts/authentication/AuthenticationService'
+
+// Styles import
+import styles from '@styles/styles';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+// Global styles
+global.styles = styles;
+global.EStyleSheet = EStyleSheet;
 
 // Global components
 global.GudText = GudText;
 global.GRadioButtonGroup = GRadioButtonGroup;
 global.ValidationField = ValidationField;
+global.CarouselCard = CarouselCard;
 
 // Global scripts
 global.AuthenticationService = AuthenticationService;
