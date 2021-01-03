4f8890a16098a779d9de72c4992177d04845ca3a
afroyogo <jaijaudenes@gmail.com>
2021-01-03 20:08:30 +0100

    Created and imported Title Card component

---
+++ components/global/TitleCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View} from 'react-native';
+import styles from '../../scss/styles';
+
+class TitleCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let title = (
+      <GudText style={styles.sectionDescription} text={this.props.title} />
+    );
+    let text = <GudText style={styles.titleCardText} text={this.props.text} />;
+    let body = this.props.body;
+
+    return (
+      <View style={styles.cardContainer}>
+        <View style={styles.cardContainerLeft}>
+          <View style={styles.flatCardItem}>
+            <View
+              style={{
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                flex: 9,
+              }}>
+              {title}
+              {text}
+              <View style={styles.flatCardItemComponent}>{body}</View>
+            </View>
+          </View>
+          <View style={styles.gudItemSeparator} />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default TitleCard;
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import { name as appName } from './app.json';
 import GudText from '@gud/GudText';
 import GRadioButtonGroup from '@gud/GRadioButtonGroup';
 import ValidationField from '@gud/ValidationField';
+import TitleCard from '@gud/TitleCard';
 
 // Scripts import
 import AuthenticationService from '@scripts/authentication/AuthenticationService'
@@ -20,12 +21,13 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 // Global styles
 global.styles = styles;
-global.EStyleSheet = EStyleSheet;
+global.ESS = EStyleSheet;
 
 // Global components
 global.GudText = GudText;
 global.GRadioButtonGroup = GRadioButtonGroup;
 global.ValidationField = ValidationField;
+global.TitleCard = TitleCard;
 
 // Global scripts
 global.AuthenticationService = AuthenticationService;
