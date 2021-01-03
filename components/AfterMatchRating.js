545b02c51d62ae346d69ccc145fdb28104b28515
afroyogo <jaijaudenes@gmail.com>
2021-01-03 16:04:10 +0100

    Finished implementing AfterMatchRating validation and communication functionalities

--- components/AfterMatchRating.js
+++ components/AfterMatchRating.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import {View, TextInput, FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
 import GRadioButtonGroup from './GRadioButtonGroup';
+import GudText from './GudText';
+
+const MIN_LENGTH = 12;
 
 class AfterMatchRating extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       ratingKey: '',
+      text: '',
     };
+    this.onChange = this.onChange.bind(this);
+    this.changeKey = this.changeKey.bind(this);
   }
 
   handleKey(key) {
     this.setState({ratingKey: key});
-    this.props.onChange({key});
+    this.props.onChange(0, key);
   }
 
+  onChange(text) {
+    var newState = this.state;
+    newState['text'] = text;
+
+    this.setState(newState);
+    this.props.onChange(1, text);
+  }
+  changeKey() {
+    this.setState({ratingKey: 0});
+  }
   render() {
+    var key = this.state.ratingKey;
+
     const MENUS = ['ratings', 'Others'];
 
     let ratings = [
@@ -37,14 +56,18 @@ class AfterMatchRating extends React.Component {
       },
     ];
 
-    let slides = [{key: 0}, {key: 1}];
+    let allSlides = [{key: 0}, {key: 1}];
+    let slides = allSlides.splice(0, key === 3 ? allSlides.length : 1);
+
     var radioButtons = (
       <View style={styles.carouselCard}>
         <GRadioButtonGroup
-          spaced={'8%'}
+          spaced={EStyleSheet.value('$gudRadioButtonMarginVertical')}
           options={ratings}
           getKey={(key) => this.handleKey(key)}
         />
+        {/* Implement paging component here */}
+        {/* {key === 3 ? <GudText style={styles.sectionDescription} text="Siguiente"/> : null} */}
       </View>
     );
     var othersBox = (
@@ -53,13 +76,15 @@ class AfterMatchRating extends React.Component {
           style={[styles.sectionDescription, {padding: 10}]}
           text={'Explícanos qué ha sucedido:'}
         />
-        <TextInput
+        <ValidationField
           style={styles.textInputContainer}
+          disableAccent={true}
+          maxLength={30}
+          minLength={MIN_LENGTH}
           autoFocus={true}
           multiline={true}
           numberOfLines={4}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
+          handleValue={(text) => this.onChange(text)}
         />
       </View>
     );
@@ -67,15 +92,22 @@ class AfterMatchRating extends React.Component {
     return (
       <View style={styles.carouselContainer}>
         <FlatList
+          ref={(ref) => {
+            this.listRef = ref;
+          }}
           data={slides}
           style={styles.carouselCard}
           keyExtractor={(item) => item.key.toString()}
           renderItem={({item}) => {
             if (item.key == 0) {
               return radioButtons;
-            } else if (item.key == 1) {
+            }
+            if (item.key == 1) {
               return othersBox;
             }
+          }}
+          onTouchMove={(event) => {
+            key = 0;
           }}
           pagingEnabled
           horizontal
