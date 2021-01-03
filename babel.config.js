62f66396b9a0e4690a987a5f5805f1ce53c464f7
afroyogo <jaijaudenes@gmail.com>
2021-01-03 16:08:44 +0100

    Created more specific route alias

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,8 @@ module.exports = {
           '@styles': './scss',
           '@assets': './assets',
           '@screens': './screens',
+          '@landing': './screens/Landing',
+          '@match': './screens/Match',
           '@android': './android/app/src/main/assets',
           '@components': './components',
           '@icons': './iconos_app',
