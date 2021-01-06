df73e48db7d0facc88aef3525379d4507af07200
afroyogo <jaijaudenes@gmail.com>
2021-01-06 15:55:10 +0100

    Created expandable menu item component (wip)

---
+++ components/global/MenuItem.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styles from '@styles/styles';
+import {View, TouchableHighlight, Image, Animated} from 'react-native';
+
+class MenuItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.icons = {
+      right: require('@icons/rightArrow.png'),
+      down: require('@icons/downArrow.png'),
+    };
+    this.state = {
+      expanded: false,
+      animation: new Animated.Value(1),
+    };
+    this.toggle.bind(this);
+  }
+  toggle() {
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+
+    // this.state.animation.setValue(initialValue); //Step 3
+    // Animated.spring(
+    //   //Step 4
+    //   this.state.animation,
+    //   {
+    //     toValue: finalValue,
+    //   },
+    // ).start(); //Step 5
+  }
+
+  render() {
+    let icon = this.icons['right'];
+
+    if (this.state.expanded) {
+      icon = this.icons['down'];
+    }
+    return (
+      <TouchableHighlight
+        style={styles.menuItemBase}
+        underlayColor={ESS.value('$gudLightGray')}
+        onPress={() => {
+          this.props.extendable
+            ? this.toggle()
+            : this.props.onClick(this.props.id);
+        }}>
+        <>
+          <View style={styles.menuItemContainer}>
+            <Image
+              style={styles.gudIcon}
+              source={this.props.icon || require('@icons/placeholder.png')}
+            />
+            <GudText style={styles.menuItemText} text={this.props.text} />
+          </View>
+          <View style={styles.gudArrowIcon}>
+            <Image source={icon} />
+          </View>
+        </>
+      </TouchableHighlight>
+    );
+  }
+}
+
+export default MenuItem;
